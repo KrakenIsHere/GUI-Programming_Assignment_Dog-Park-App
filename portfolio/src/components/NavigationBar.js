e3f9e38985d585af106020af9d7b561c7cade74b
Kristian K. S. Nielsen <kris9463@edu.sde.dk>
2019-02-21 14:38:30 +0100

    stuff n things

---
+++ portfolio/src/components/NavigationBar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { NavLink } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Navigation = () => (
+
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <NavLink className="navbar-brand" to="/">ESS</NavLink>
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="nav-link active" exact={true} to="/">Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="nav-link active" to="/about">About</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="nav-link active" to="/contact">Contact</NavLink>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+  );
+
+  export default Navigation;
